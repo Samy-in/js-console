02da6ea1374362065a9f8e807565e731b2b68546
Samy-in <141911040+Samy-in@users.noreply.github.com>
2024-06-21 19:16:34 +0000

    Functions

--- Basics/Functions.js
+++ Basics/Functions.js
@@ -53,6 +53,87 @@ function InstaMartprice(val1, val2, ...num1) {
 console.log(InstaMartprice(564, 58, 7876, 35));  //When we  print this num1 only last two values is inside the variable
                                     // Because first two values is taken out and stored in different variable
 
+// We can also access object inside a function
+
+const User = {
+      Username : "Jitendra",
+      Id : 28439
+
+}
+function handleObject (anyobject) {  // we have to write anyobject to allow object inside the function
+              console.log(`Username is ${anyobject.Username} and id is ${anyobject.Id}`);
+}
+console.log(handleObject(User))
+handleObject(User)
+              // We can also write object inside this function which changes the values
+              handleObject({
+                Username : "Milenge",
+                Id : 4637
+              })
+// this has changed the value from Jitendra to Milenge 
+const MynewArray = [478, 9854, 945, 450]
+
+// then we store this object into an function
+function ReturnSecondvalue(myArray) {
+                          return myArray[3]
+}
+console.log(ReturnSecondvalue(MynewArray));
+
+// ********************************* Global and local Scopes ******************************************
+
+{}  //This is called scope and it can be scope of any function or object
+// Whatever you write outside of this brackets are called global scope i.e. there scope is globally available
+// And this which are written inside is block scope and hese things can't be accessed by outside refering to it
+
+let a = 48
+let b = 74   // This are global scope values assigned to a,b,c
+let c = 83
+
+if (true) {
+ let b = 23
+ const c = 84                               //This is called block scope
+console.log("Inner scope's value =", b,c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
+   // ***** this scope environment in this node.js and in browser's inspect console is different ******** \\
+
+// Scope's of Function inside functions.
+function one() {
+   let OppTeam = "Germany"
+
+                            // This function two() can also be considered as child part of parential function one()
+                               // that is why child class can access the keys in parent class but parent can't access 
+   function two() {                           //this keys of child class(Ex. firstteam etc)
+          const Firstteam = "England"
+          console.log(OppTeam);
+         }
+         two()   // this is executing this function
+         //console.log(Firstteam);      //*** This creates error as 
+                                      // we can't access Firstteam because its out of this scope of function two() ***\\
+    }
+
+one()   // this is executing this function
+
+//+++++++++++++++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++++++++++++++++++\\ 
+// we are delcaring function in two ways 
+// 1.) by regular way
+function additionOne (num1) {
+    return num1 + 1
+}
+console.log(additionOne(34));  // This printing staement can be written even before ths declsring of this function
+                  // this is the main advantages
+
+// 2.) by hosting way
+// Where we store his function into an variable
+
+const Boredlife = function(num1) {
+    return num1 + 2
+}
+console.log(Boredlife(4)) // here the disadvange is that you can't write this printing statement before function declaration
+
+
 
 
 
