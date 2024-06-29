cc80373cb2a12ba6ad9dac8c2075c871af243e78
Samy-in <rojoldashec34@gmail.com>
2024-06-30 00:20:43 +0530

    All about types of Dom selector

---
+++ Backend and Projects/Domintro.js
@@ -0,0 +1,14 @@
+
+// DOM = Document Object Model.
+// So we know in console of an browser if we console.log(window) this shows everything like our location,type of the document
+// etc. so we need tofocus on printing console.dir(documents) = to see all the hidden documents.
+
+//1. document.getElementById('any name inside an element or class name') this is used in console to know.
+
+
+
+
+
+
+
+
