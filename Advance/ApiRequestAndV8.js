75dfe8be619e28962af1a4ee6b104e61c4239d4b
Samy-in <rojoldashec34@gmail.com>
2024-07-08 21:23:44 +0530

    All about Xmlhttprequests

---
+++ Advance/ApiRequestAndV8.js
@@ -0,0 +1,28 @@
+// 
+// API stands for “application programming interface.” An API is essentially a set of rules that dictate 
+// how two machines talk to each other. Some examples of API-based interactions include a 
+// cloud application communicating with a server, servers pinging each other, 
+// or applications interacting with an operating system.
+// 
+// we take api's which are big codes and use "Json formatter" to interpret this api code.
+// 
+// Before fetch() which is used to take apis or files from different environment or database.
+// We used to take Xmlhttprequests() there are different types as well. 
+// 
+// xmlhttprequest is AJAX(Asychronous javascript and XML) related programming 
+// 
+// there are Xmlhttprequest.readystate() which has five types of states.
+// srno | Value | State
+//      |   0   | UNSENT = here client has been created but open() function is not called.
+//      |   1   | OPENED = here open() is called meanns request is sent but data is not received.
+//      |   2   | HEADERS RECEIVED = here send() is called means data is given but not in systematically with no status.
+//      |   3   | LOADING = here responese are aken and its data.
+//      |   4   | DONE = operation is done.
+// 
+// we always write xmlhttprequest() as a new object like "const Objectname = new Xmlhttprequest()"
+// to request we use Objectname.open("inside we have write method like= GET,POST,PUT,PUSH for the files",
+                      //             And need to mention a variable who has url in a string input.)
+// 
+// 
+// 
+// 
